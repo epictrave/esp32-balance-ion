e059079a2f17ff56336cee413fe8bce0d4fa2d78
epictrave <flacks12@gmail.com>
2020-02-10 09:41:16 +0900

    feat: change queue-message path

--- component.mk
+++ component.mk
@@ -1,8 +1,8 @@
 COMPONENT_SRCDIRS = . \
 deps/parson \
-deps/queue_message 
+deps/esp32-queue-message 
 
 COMPONENT_ADD_INCLUDEDIRS = . \
 deps \
 deps/parson \
-deps/queue_message 
+deps/esp32-queue-message 
